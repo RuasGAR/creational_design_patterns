71f8ff1d27cac2e607de2e1c633328598336d6e7
Gabriel Ruas <gabrielamaralruas@gmail.com>
2026-01-06 14:45:31 -0300

    +LazyLoading

---
+++ Singleton/SingletonLazyLoading/Program.cs
@@ -0,0 +1,28 @@
+﻿Singleton singleton = Singleton.Instance;
+
+sealed class Singleton
+{
+
+    private static Singleton _instance = default!;
+
+    public static Singleton Instance 
+    {
+        get
+        {
+            if(_instance is null) // checks if it's the very first time the instance is requested
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    
+    }
+
+    private Singleton()
+    {
+    }
+}
+
+// This implemenation is not thread-safe. In a multi-threaded environment, multiple threads could simultaneously check if _instance
+// is null and create multiple instances of the Singleton class.
---
+++ Singleton/SingletonLazyThreadSafe/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+sealed class Singleton { 
+
+    private static Singleton _instance = default!;
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if(_instance is null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    
+    }
+
+
+    private Singleton()
+    {
+    }
+
+
+}
